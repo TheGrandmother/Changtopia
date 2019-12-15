d02601badaf985e02b12842f6ac120dd1a5cf929
Henrik Sommerland <henrik.sommerland@gmail.com>
2019-12-15 14:55:21 +0100

    Reverting to xxhashjs due to module incompatible with workers

--- util/hash.js
+++ util/hash.js
@@ -1,5 +1,9 @@
-const XXHash = require('xxhash')
-const hash = (val) => XXHash.hash(Buffer.from(val.toString()), 0xcafebabe)
+const XXH = require('xxhashjs')
+// We have to use xxhashjs since cchash doesnt play with workers.
+// If this is resolved we should switch back due to next level
+// speed increase
+
+const hash = (val) => Number(XXH.h32(val.toString(), 0xcafebabe ))
 const randomHash = () => hash((Math.random() * Number.MAX_SAFE_INTEGER).toString())
 
 module.exports = {
